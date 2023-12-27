1305d864d867272653ca8fb6c42a8237e970dff9
Ammar Hammad <amargamal832909@gmail.com>
2023-12-27 16:12:26 +0200

    This is for Day01 in Csharp Exploring DotNet Framework

---
+++ CsharpOOP/Day01/Day01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine($"Default Value of Byte: {default(byte)} ");
+        Console.WriteLine($"Default Value of Integer: {default(int)}");
+        Console.WriteLine($"Default Value of Float: {default(float)}");
+        Console.WriteLine($"Default Value of Long: {default(long)}");
+        Console.WriteLine($"Default Value of Double: {default(double)}");
+        Console.WriteLine($"Default Value of Character: {default(char)}");
+        Console.WriteLine($"Default Value of Boolean: {default(bool)}");
+    }
+}
