e317ac7aed2b53a857585b6ba650b18b6e8511d1
Ammar Hammad <amargamal832909@gmail.com>
2024-01-25 15:31:08 +0200

    Fix line endings

---
+++ EntityFramework/EFCore/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace EFCore
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Hello World");
+
+            Console.ReadKey();
+        } 
+    }
+}
